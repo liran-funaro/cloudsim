69b8221884579bacbc21ff4cbe472345fa1ebe7b
Liran Funaro <fonaro@gmail.com>
2019-11-15 21:10:53 +0200

    nesteddict moved to github

--- setup.py
+++ setup.py
@@ -39,5 +39,5 @@ setup(
     ],
     install_requires=['numpy', 'msgpack', 'msgpack-numpy', 'matplotlib', 'scipy', 'seaborn', 'sympy', 'pandas',
                       'nesteddict', 'tabulate', 'requests', 'pygments'],
-    dependency_links=['https://bitbucket.org/funaro/nesteddict']
+    dependency_links=['https://github.com/liran-funaro/nesteddict']
 )
